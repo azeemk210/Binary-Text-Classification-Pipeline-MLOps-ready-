a3fe2d78a7995e3010ee346616241128cc4c2c9a
azeemk210 <khan.azeemak@gmail.com>
2026-01-21 21:58:41 +0100

    changed max_features to 45 from 50

--- src/feature_engineering.py
+++ src/feature_engineering.py
@@ -77,7 +77,7 @@ def save_data(df: pd.DataFrame, file_path: str) -> None:
 
 def main():
     try:
-        max_features = 50
+        max_features = 45
         train_data = load_data('./data/interim/train_processed.csv')
         test_data = load_data('./data/interim/test_processed.csv')
 
